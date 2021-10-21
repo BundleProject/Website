f09cc8c7f911ca7def928628969d9974d01d5589
isXander <xandersmith2008@gmail.com>
2021-10-21 21:40:47 +0100

    added dark mode

---
+++ darkmode.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    let theme = localStorage.getItem("theme")
+
+    if (theme === null) {
+        theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+        localStorage.setItem("theme", theme)
+    }
+
+    setTheme(theme)
+
+    $('#dark-mode-switch').click(toggleTheme);
+})
+
+function setTheme(theme) {
+    let body = $(document.body)
+
+    if (theme === "dark") {
+        body.removeClass("theme--dark")
+        body.removeClass("theme--light")
+
+        body.addClass("theme--dark")
+    } else if (theme === "light") {
+        body.removeClass("theme--dark")
+        body.removeClass("theme--light")
+
+        body.addClass("theme--light")
+    }
+}
+
+function toggleTheme() {
+    let theme = localStorage.getItem("theme")
+
+    if (theme === "dark") theme = "light"
+    else if (theme === "light") theme = "dark"
+    else theme = "dark"
+
+    localStorage.setItem("theme", theme)
+
+    setTheme(theme)
+}
